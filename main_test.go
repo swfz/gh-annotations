8913cb31b83f3e619aba741c0a8398425852fad4
swfz <sawafuji.09@gmail.com>
2023-03-30 04:39:31 +0900

    test: テストケースごとにmockレスポンスを用意するようにした

--- main_test.go
+++ main_test.go
@@ -18,8 +18,22 @@ func Test_run(t *testing.T) {
 		wantErrOut string
 	}{
 		{
-			name:    "Standard Output",
+			name:    "1workflow, 1job, 1annotation",
 			options: Options{},
+			stubs: func(reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.REST("GET", "repos/swfz/gh-annotations/actions/runs"),
+					httpmock.FileResponse("./fixtures/workflow_run.json"),
+				)
+				reg.Register(
+					httpmock.REST("GET", "repos/swfz/gh-annotations/actions/runs/1001/jobs"),
+					httpmock.FileResponse("./fixtures/runs_1001_jobs.json"),
+				)
+				reg.Register(
+					httpmock.REST("GET", "repos/swfz/gh-annotations/check-runs/10001/annotations"),
+					httpmock.FileResponse("./fixtures/check_runs_10001_annotations.json"),
+				)
+			},
 			wantOut: heredoc.Doc(`
 Repository           Workflow  Event  Job         JobStartedAt          JobCompletedAt        Conclusion  AnnotationLevel  Message
 swfz/gh-annotations  Run 1001  push   Sample Job  2023-03-20T10:00:00Z  2023-03-20T10:02:00Z  success     warning          This is a sample annotation
@@ -32,6 +46,8 @@ swfz/gh-annotations  Run 1001  push   Sample Job  2023-03-20T10:00:00Z  2023-03-
 			reg := &httpmock.Registry{}
 			defer reg.Verify(t)
 			if tt.stubs == nil {
+				// TODO: すべてからレスポンスのほうがいいかも
+				// TODO: 正規表現行けるなら指定してすべてのリクエストを差し替える
 				reg.Register(
 					httpmock.REST("GET", "repos/swfz/gh-annotations/actions/runs"),
 					httpmock.FileResponse("./fixtures/workflow_run.json"),
